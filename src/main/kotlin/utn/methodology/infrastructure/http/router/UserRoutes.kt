9127cd9de668d9a7433cd5ceeaa14f68e08860a0
geronimo-03 <129116160+geronimo-03@users.noreply.github.com>
2024-11-07 18:09:39 -0300

    UserRouter.kt
    Agrego la ruta para la creacion de un usuario

---
+++ src/main/kotlin/utn/methodology/infrastructure/http/router/UserRoutes.kt
@@ -0,0 +1,36 @@
+package utn.methodology.infrastructure.http.router
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import utn.methodology.application.commandhandlers.CreateUserCommandHandler
+import utn.methodology.infrastructure.http.actions.ConfirmUserAction
+import utn.methodology.application.commands.CreateUserCommand
+import utn.methodology.infrastructure.persistence.connectToMongoDB
+import utn.methodology.infrastructure.persistence.UserRepository
+import utn.methodology.application.commands.ActualizarUsuarioCommand
+import utn.methodology.application.commandhandlers.ActualizarUsuarioCommandHandler
+
+
+
+fun Application.userRoutes() {
+    val mongoDatabase = connectToMongoDB()
+    val userRepository = UserRepository(mongoDatabase)
+    val confirmUserAction = ConfirmUserAction(CreateUserCommandHandler(userRepository))
+
+    routing {
+        post("/users") {
+            val body = call.receive<CreateUserCommand>()
+
+            try {
+                body.validate() // Validar aquí
+                confirmUserAction.execute(body)
+                call.respond(HttpStatusCode.Created, mapOf("message" to "User Created Successfully"))
+            } catch (e: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest, mapOf("error" to e.message))
+            }
+        }
+    }
+}
