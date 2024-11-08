fa66d1aee3024f09444b0b84c6da0e01493aec62
Andres Schneider <130928583+LiLFogg@users.noreply.github.com>
2024-11-08 19:31:44 -0300

    Update ApplicationTest.kt

--- src/test/kotlin/utn/methodology/ApplicationTest.kt
+++ src/test/kotlin/utn/methodology/ApplicationTest.kt
@@ -6,12 +6,20 @@ import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
+
+
+
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
+        application {
+            testModule() // Llama a testModule que ahora invoca module()
+        }
         client.get("/").apply {
+            println("Response: ${bodyAsText()}") // Esto te mostrará la respuesta
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
         }
+
     }
 }
